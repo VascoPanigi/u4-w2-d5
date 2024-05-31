67350eece40daa3c6636a1dcfa002ce5e68b4250
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-05-31 13:21:49 +0200

    implement add functionality

--- src/main/java/VascoPanigi/Application.java
+++ src/main/java/VascoPanigi/Application.java
@@ -1,8 +1,45 @@
 package VascoPanigi;
 
+
+import VascoPanigi.entities.Book;
+import VascoPanigi.entities.Catalogue;
+import VascoPanigi.entities.Magazine;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static List<Catalogue> catalogue = new ArrayList<>();
+    static List<Book> books = new ArrayList<>();
+    static List<Magazine> magazines = new ArrayList<>();
+
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        initializeCatalogue();
+
+        System.out.println(catalogue);
     }
+
+    public static void initializeCatalogue() {
+        generateBooks();
+        generateMagazines();
+
+        catalogue.addAll(books);
+        catalogue.addAll(magazines);
+    }
+
+    public static void generateBooks() {
+        for (int i = 0; i < 10; i++) {
+            Book book = new Book(Catalogue.generateTotalPages(), Catalogue.generateYear(), Catalogue.generateTitle(), Catalogue.generateIsbn(), Book.generateGenre(), Book.generateAuthor());
+            books.add(book);
+        }
+    }
+
+    public static void generateMagazines() {
+        for (int i = 0; i < 10; i++) {
+            Magazine magazine = new Magazine(Catalogue.generateTotalPages(), Catalogue.generateYear(), Catalogue.generateTitle(), Catalogue.generateIsbn(), Magazine.generatePeriodicity());
+            magazines.add(magazine);
+        }
+    }
+
 }
